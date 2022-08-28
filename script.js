40197f4e320fa2d9471db4bfb52eb6418f63ae94
rachael-pluta <rachael.pluta.work@gmail.com>
2022-08-28 15:29:03 +1000

    created password variables and random.math function to create password

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const number = "1234567890";
+const symbol = "!@#$%^&*()";
 
 
 // Get references to the #generate element
@@ -13,5 +17,39 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var finalPassword = "";
+  var passwordSize = prompt("How many characters do you want for your password?");
+  if (passwordSize >= 8 && passwordSize <= 128) {
+    var checkLowerCase = confirm("Do you want lowercase characters?");
+    var checkUpperCase = confirm("Do you want uppercase characters?");
+    var checkNumber = confirm("Do you want numbers?");
+    var checkSymbol = confirm("Do you want symbols?");
+    for (var i = 0; i < passwordSize; i++) {
+      if (checkLowerCase === true && finalPassword.length < passwordSize) {
+        var position = Math.floor(Math.random() * 26);
+        finalPassword = finalPassword + lowerCase[position];
+      }
+      if (checkUpperCase === true && finalPassword.length < passwordSize) {
+        var position = Math.floor(Math.random() * 26);
+        finalPassword = finalPassword + upperCase[position];
+      }
+      if (checkNumber === true && finalPassword.length < passwordSize) {
+        var position = Math.floor(Math.random() * 10);
+        finalPassword = finalPassword + number[position];
+      }
+      if (checkSymbol === true && finalPassword.length < passwordSize) {
+        var position = Math.floor(Math.random() * 10);
+        finalPassword = finalPassword + symbol[position];
+      }
+    }
+  }
+  else {
+    alert("Invalid entry.")
+  }
+  return finalPassword
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
